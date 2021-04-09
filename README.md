5ad99bec1794531eeb8b14667ce81aef800591df
helywin <jiang770882022@hotmail.com>
2021-04-09 17:14:26 +0800

    add readme, add set_simple_meta_info macro, add dot files

---
+++ README.md
@@ -0,0 +1,14 @@
+# CMake Modular Template
+
+## Features
+
+- generate libraries and executables
+- generate RC file (Windows)
+- auto update build time
+- read version from file
+- fetch git repo info
+- generate dll export header (Windows)
+
+## Status
+- [x] Windows
+- [ ] Linux
--- cmake/Submodule.cmake
+++ cmake/Submodule.cmake
@@ -1,5 +1,6 @@
 include(CMakePrintHelpers)
 include(GenerateExportHeader)
+include(GenerateMetaInfo)
 include(ReadVersion)
 set(MODULE_LIST "")
 
@@ -58,4 +59,16 @@ macro(set_submodule_executable PREFIX_NAME_IN MODULE_NAME_IN)
             NAMESPACE ${CURRENT_PREFIX_NAME}
             )
     append_module_list()
+endmacro()
+
+macro(set_simple_meta_info)
+    if (WIN32)
+        generate_meta_info(
+                META_FILE
+                MODULE_NAME ${CURRENT_MODULE_NAME}
+                PRODUCT_NAME ${CURRENT_PREFIX_NAME}
+                ORIGINAL_FILE_NAME ${CURRENT_PREFIX_NAME}.${CURRENT_MODULE_NAME}
+                PRODUCT_VERSION ${GENERAL_VERSION})
+        target_sources(${CURRENT_MODULE_TARGET} PRIVATE ${META_FILE})
+    endif ()
 endmacro()
